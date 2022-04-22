f0f84f77c82449a9102010024adc8dbc3ec887d9
charles johnston <charlesjohnston2002@yahoo.com>
2022-04-22 11:02:13 -0400

    added server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+// Requirements (express and mongoose)
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// Connection for mongoose
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/cj-social-network-api-3', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Use this to log mongo queries being executed
+mongoose.set('debug', true);
+
+app.use(require('./routes'));
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
