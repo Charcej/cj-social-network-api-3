d7288b21877cee58f9a0d94a3f17b8e525a8424b
charles johnston <charlesjohnston2002@yahoo.com>
2022-04-22 12:55:21 -0400

    added api folder and index.js for that folder

---
+++ routes/api/index.js
@@ -0,0 +1,15 @@
+// Requirement for express router
+const router = require('express').Router();
+
+// Set routes for users and thoughts
+const usersRoutes = require('./user-routes');
+const thoughtsRoutes = require('./thought-routes');
+
+// Add the prefix `/users` to routes as created
+router.use('/users', usersRoutes);
+
+// Add the prefix `/thoughts` to routes as created 
+router.use('/thoughts', thoughtsRoutes);
+
+// Export module router
+module.exports = router;
